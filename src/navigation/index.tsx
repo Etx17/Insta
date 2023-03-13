622ae0686b65745711ebc7080eb593bbd0c7fc76
Etx17 <etiennededi@hotmail.fr>
2023-03-13 13:06:01 +0100

    navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -4,30 +4,30 @@ import ProfileScreen from '../screens/ProfileScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Image } from 'react-native';
 import instalogo from '../assets/images/instalogo.png';
+import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Feed" screenOptions={{headerShown: true}}>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: true}} >
+
                 <Stack.Screen 
-                    name="Feed" 
-                    component={HomeScreen}
-                    options={{ headerTitle: () => <HeaderTitle />, headerTitleAlign: 'center' }}
+                    name="Home" 
+                    component={BottomTabNavigator} 
+                    options={{headerShown: false}} 
                 />
+
                 <Stack.Screen 
                     name="UserProfile" 
                     component={ProfileScreen} 
+                    options={{title: 'Profile'}}
                 />
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
 
-const HeaderTitle = () => {
-    return (
-       <Image source={instalogo} resizeMode="contain" style={{width: 150, height: 40}}/>
-    )
-}
+
 
 export default Navigation;
