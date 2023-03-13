d226c986391266f87fde79cdb11f474891687895
Etx17 <etiennededi@hotmail.fr>
2023-03-13 18:09:18 +0100

    amplify config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
-import {View, StyleSheet} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig)
+
 const App = () => {
   return (
     <SafeAreaProvider>
